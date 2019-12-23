2a6f49a4f144de114eabc1f657485f2729ce1fa8
Doron Adler <doron@glidetalk.com>
2019-12-23 15:18:52 +0200

    * Updated OpenSSL build script to revision 17 https://gist.github.com/Norod/2f3ef9b6e3758dfc1433/revisions#diff-d18bc4cbac191faf4e49f68843e682ea * Updated OpenSSL build script to build for only 64bit architectures * Updated Scheme & Project settings

--- OpenSSL/build-OpenSSL-iOS-Mac.sh
+++ OpenSSL/build-OpenSSL-iOS-Mac.sh
@@ -10,6 +10,7 @@
 # Doron Adler, GlideTalk, @Norod78
 
 # Updated to work with Xcode 8 and iOS 10
+# Updated to build for only 64bit architectures
 
 set -e
 
@@ -22,41 +23,44 @@ OPENSSL_VERSION="openssl-1.0.2d"
 ###################################
 # 		 SDK Version
 ###################################
-SDK_VERSION=$(xcodebuild -version -sdk iphoneos | grep SDKVersion | cut -f2 -d ':' | tr -d '[[:space:]]')
+IOS_SDK_VERSION=$(xcodebuild -version -sdk iphoneos | grep SDKVersion | cut -f2 -d ':' | tr -d '[[:space:]]')
 ###################################
 
 ################################################
 # 		 Minimum iOS deployment target version
 ################################################
-MIN_IOS_VERSION="8.0"
+MIN_IOS_VERSION="10.0"
+
+################################################
+# 		 Minimum OS X deployment target version
+################################################
+MIN_OSX_VERSION="10.12"
 
 echo "----------------------------------------"
 echo "OpenSSL version: ${OPENSSL_VERSION}"
-echo "iOS SDK version: ${SDK_VERSION}"
+echo "iOS SDK version: ${IOS_SDK_VERSION}"
 echo "iOS deployment target: ${MIN_IOS_VERSION}"
+echo "OS X deployment target: ${MIN_OSX_VERSION}"
 echo "----------------------------------------"
 echo " "
 
 DEVELOPER=`xcode-select -print-path`
-
 buildMac()
 {
 	ARCH=$1
-
 	echo "Start Building ${OPENSSL_VERSION} for ${ARCH}"
-
 	TARGET="darwin-i386-cc"
-
 	if [[ $ARCH == "x86_64" ]]; then
 		TARGET="darwin64-x86_64-cc"
 	fi
 	
-	export CC="${BUILD_TOOLS}/usr/bin/clang"
-
+	export CC="${BUILD_TOOLS}/usr/bin/clang -mmacosx-version-min=${MIN_OSX_VERSION}"
 	pushd . > /dev/null
 	cd "${OPENSSL_VERSION}"
+	pwd
 	echo "Configure"
 	./Configure ${TARGET} --openssldir="/tmp/${OPENSSL_VERSION}-${ARCH}" &> "/tmp/${OPENSSL_VERSION}-${ARCH}.log"
+	echo "make"
 	make >> "/tmp/${OPENSSL_VERSION}-${ARCH}.log" 2>&1
 	echo "make install"
 	make install >> "/tmp/${OPENSSL_VERSION}-${ARCH}.log" 2>&1
@@ -66,12 +70,10 @@ buildMac()
 	
 	echo "Done Building ${OPENSSL_VERSION} for ${ARCH}"
 }
-
 buildIOS()
 {
 	ARCH=$1
-
-	echo "Start Building ${OPENSSL_VERSION} for ${PLATFORM} ${SDK_VERSION} ${ARCH}"
+	echo "Start Building ${OPENSSL_VERSION} for ${PLATFORM} ${IOS_SDK_VERSION} ${ARCH}"
 	
 	pushd . > /dev/null
 	cd "${OPENSSL_VERSION}"
@@ -85,12 +87,12 @@ buildIOS()
   
 	export $PLATFORM
 	export CROSS_TOP="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
-	export CROSS_SDK="${PLATFORM}${SDK_VERSION}.sdk"
+	export CROSS_SDK="${PLATFORM}${IOS_SDK_VERSION}.sdk"
 	export BUILD_TOOLS="${DEVELOPER}"
 	export CC="${BUILD_TOOLS}/usr/bin/gcc -fembed-bitcode -mios-version-min=${MIN_IOS_VERSION} -arch ${ARCH}"
 	
+	pwd
 	echo "Configure"
-
 	if [[ "${ARCH}" == "x86_64" ]]; then
 		./Configure darwin64-x86_64-cc --openssldir="/tmp/${OPENSSL_VERSION}-iOS-${ARCH}" &> "/tmp/${OPENSSL_VERSION}-iOS-${ARCH}.log"
 	else
@@ -98,7 +100,6 @@ buildIOS()
 	fi
 	# add -isysroot to CC=
 	sed -ie "s!^CFLAG=!CFLAG=-isysroot ${CROSS_TOP}/SDKs/${CROSS_SDK} -mios-version-min=${MIN_IOS_VERSION} !" "Makefile"
-
 	echo "make"
 	make >> "/tmp/${OPENSSL_VERSION}-iOS-${ARCH}.log" 2>&1
 	echo "make install"
@@ -109,70 +110,46 @@ buildIOS()
 	
 	echo "Done Building ${OPENSSL_VERSION} for ${ARCH}"
 }
-
 echo "Cleaning up"
 rm -rf include/openssl/* lib/*
 rm -rf /tmp/${OPENSSL_VERSION}-*
 rm -rf ${OPENSSL_VERSION}
-
 mkdir -p lib/iOS
 mkdir -p lib/Mac
 mkdir -p include/openssl/
-
 rm -rf "/tmp/${OPENSSL_VERSION}-*"
 rm -rf "/tmp/${OPENSSL_VERSION}-*.log"
-
 rm -rf "${OPENSSL_VERSION}"
-
 if [ ! -e ${OPENSSL_VERSION}.tar.gz ]; then
 	echo "Downloading ${OPENSSL_VERSION}.tar.gz"
 	curl -O https://www.openssl.org/source/${OPENSSL_VERSION}.tar.gz
 else
 	echo "Using ${OPENSSL_VERSION}.tar.gz"
 fi
-
 echo "Unpacking openssl"
 tar xfz "${OPENSSL_VERSION}.tar.gz"
-
-buildMac "i386"
 buildMac "x86_64"
-
 echo "Copying headers"
-cp /tmp/${OPENSSL_VERSION}-i386/include/openssl/* include/openssl/
-
+cp /tmp/${OPENSSL_VERSION}-x86_64/include/openssl/* include/openssl/
 echo "Building Mac libraries"
 lipo \
-	"/tmp/${OPENSSL_VERSION}-i386/lib/libcrypto.a" \
 	"/tmp/${OPENSSL_VERSION}-x86_64/lib/libcrypto.a" \
 	-create -output lib/Mac/libcrypto.a
-
 lipo \
-	"/tmp/${OPENSSL_VERSION}-i386/lib/libssl.a" \
 	"/tmp/${OPENSSL_VERSION}-x86_64/lib/libssl.a" \
 	-create -output lib/Mac/libssl.a
-
-buildIOS "armv7"
 buildIOS "arm64"
 buildIOS "x86_64"
-buildIOS "i386"
-
 echo "Building iOS libraries"
 lipo \
-	"/tmp/${OPENSSL_VERSION}-iOS-armv7/lib/libcrypto.a" \
 	"/tmp/${OPENSSL_VERSION}-iOS-arm64/lib/libcrypto.a" \
-	"/tmp/${OPENSSL_VERSION}-iOS-i386/lib/libcrypto.a" \
 	"/tmp/${OPENSSL_VERSION}-iOS-x86_64/lib/libcrypto.a" \
 	-create -output lib/iOS/libcrypto.a
-
 lipo \
-	"/tmp/${OPENSSL_VERSION}-iOS-armv7/lib/libssl.a" \
 	"/tmp/${OPENSSL_VERSION}-iOS-arm64/lib/libssl.a" \
-	"/tmp/${OPENSSL_VERSION}-iOS-i386/lib/libssl.a" \
 	"/tmp/${OPENSSL_VERSION}-iOS-x86_64/lib/libssl.a" \
 	-create -output lib/iOS/libssl.a
-
 echo "Cleaning up"
 rm -rf /tmp/${OPENSSL_VERSION}-*
 rm -rf ${OPENSSL_VERSION}
-
 echo "Done"
